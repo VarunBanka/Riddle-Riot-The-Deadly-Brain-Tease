f68c4f176ada350a0b25ff35d6d45b25e1811b53
Varun Banka <bankavarun18@gmail.com>
2023-12-07 20:59:00 +0530

    added itsNerd & bg to page 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,101 @@
 import 'package:flutter/material.dart';
-   import 'package:chemistry_game/page1.dart';
-   import 'package:chemistry_game/page2.dart';
-   import 'package:chemistry_game/page3.dart';
-   import 'package:chemistry_game/page4.dart';
+import 'package:chemistry_game/page1.dart';
+import 'package:chemistry_game/page2.dart';
+import 'package:chemistry_game/page3.dart';
+import 'package:chemistry_game/page4.dart';
 
-   void main() {
-     runApp(MyApp());
-   }
+void main() {
+  runApp(MyApp());
+}
 
-   class MyApp extends StatelessWidget {
-     @override
-     Widget build(BuildContext context) {
-       return MaterialApp(
-         home: MyHomePage(),
-         routes: {
-           '/page1': (context) => Page1(),
-           '/page2': (context) => Page2(),
-           '/page3': (context) => Page3(),
-           '/page4': (context) => Page4(),
-         },
-       );
-     }
-   }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      routes: {
+        '/page1': (context) => Page1(),
+        '/page2': (context) => Page2(),
+        '/page3': (context) => Page3(),
+        '/page4': (context) => Page4(),
+      },
+    );
+  }
+}
 
-   class MyHomePage extends StatelessWidget {
-     @override
-     Widget build(BuildContext context) {
-       return Scaffold(
-         appBar: AppBar(
-           title: Text('Your App Name'),
-         ),
-         body: Stack(
-           fit: StackFit.expand,
-           children: <Widget>[
-             Image.asset(
-               'assets/background_image.png', // Replace with the path to your background image
-               fit: BoxFit.cover,
-             ),
-             Center(
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: <Widget>[
-                   ElevatedButton(
-                     onPressed: () {
-                       Navigator.pushNamed(context, '/page1');
-                     },
-                     child: Text('Button 1'),
-                   ),
-                   SizedBox(height: 20),
-                   ElevatedButton(
-                     onPressed: () {
-                       Navigator.pushNamed(context, '/page2');
-                     },
-                     child: Text('Button 2'),
-                   ),
-                   SizedBox(height: 20),
-                   ElevatedButton(
-                     onPressed: () {
-                       Navigator.pushNamed(context, '/page3');
-                     },
-                     child: Text('Button 3'),
-                   ),
-                   SizedBox(height: 20),
-                   ElevatedButton(
-                     onPressed: () {
-                       Navigator.pushNamed(context, '/page4');
-                     },
-                     child: Text('Button 4'),
-                   ),
-                 ],
-               ),
-             ),
-           ],
-         ),
-       );
-     }
-   }
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riddle Riot: The Deadly Brain Tease'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (ctx) => const Text("Made by Varun Banka"),
+              );
+            },
+            icon: const Icon(Icons.developer_board),
+          ),
+        ],
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'assets/background_image.png', // Replace with the path to your background image
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/page1');
+                  },
+                  child: const Text('Level 1'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigator.pushNamed(context, '/page2');
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (ctx) => const Text("Please start with Level 1"),
+                    );
+                  },
+                  child: const Text('Level 2'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigator.pushNamed(context, '/page3');
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (ctx) => const Text("Please start with Level 1"),
+                    );
+                  },
+                  child: const Text('Level 3'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigator.pushNamed(context, '/page4');
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (ctx) => const Text("Please start with Level 1"),
+                    );
+                  },
+                  child: const Text('Level 4'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/page1.dart
+++ lib/page1.dart
@@ -5,10 +5,32 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Page 1'),
+        title: const Text('Page 1'),
       ),
-      body: Center(
-        child: Text('This is Page 1'),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                'assets/background_image.png'), // Replace with the path to your background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'This is Page 1',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // Add your buttons here
+            ],
+          ),
+        ),
       ),
     );
   }
